a403c50da106b2fd07dc25fcb806eb26a1f057cd
linjoehan <linjoehan@yahoo.com>
2021-04-22 22:58:24 +0200

    Gets all files in folder for processing

--- cpp_solution/src/main.cpp
+++ cpp_solution/src/main.cpp
@@ -2,12 +2,34 @@
 #include <vector>
 #include <fstream>
 #include <string>
-#include <stdlib.h>
-#include <stdio.h>
+#include <windows.h>
+
 #include "libpq-fe.h"
+
+//Function used to get all files 
+//Reference: https://stackoverflow.com/questions/612097/how-can-i-get-the-list-of-files-in-a-directory-using-c-or-c
+std::vector<std::string> get_all_files_names_within_folder(std::string folder)
+{
+    std::vector<std::string> names;
+    std::string search_path = folder + "/*.*";
+    WIN32_FIND_DATA fd; 
+    HANDLE hFind = ::FindFirstFile(search_path.c_str(), &fd); 
+    if(hFind != INVALID_HANDLE_VALUE) { 
+        do { 
+            // read all (real) files in current folder
+            // , delete '!' read other 2 default folder . and ..
+            if(! (fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) ) {
+                names.push_back(fd.cFileName);
+            }
+        }while(::FindNextFile(hFind, &fd)); 
+        ::FindClose(hFind); 
+    } 
+    return names;
+}
 
 int main()
 {
+    //Establish connection to database
     const char *conninfo;
     PGconn *conn;
     
@@ -23,7 +45,17 @@ int main()
     
     std::cout << "Sucessfully connected to database" << std::endl;
     
+    //Process all Region data files
+    std::string file_path = "./Data/Region/";
+    std::vector<std::string> files = get_all_files_names_within_folder(file_path);
+    std::cout << "Region files:" << files.size() << std::endl;
+    
+    
     PQfinish(conn);
+    
     
     return 0;
 }
+
+
+
