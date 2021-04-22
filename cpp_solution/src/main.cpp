032177c51bd9b8a0b88a3a729565357feda17928
linjoehan <linjoehan@yahoo.com>
2021-04-22 23:05:53 +0200

    added function to move files to rejected or arhived folder

--- cpp_solution/src/main.cpp
+++ cpp_solution/src/main.cpp
@@ -27,6 +27,36 @@ std::vector<std::string> get_all_files_names_within_folder(std::string folder)
     return names;
 }
 
+//Move rejected file to rejected folder or completed file to archive folder
+int move_file(std::string filepath,std::string filename,std::string move_type)
+{
+    std::string target_path;
+    if(move_type == "reject")
+    {
+        target_path = "./Data/Rejected/";
+    }
+    else if (move_type == "archive")
+    {
+        target_path = "./Data/Archive/";
+    }
+    else
+    {
+        std::cerr << "Unknown move type:" << move_type << " Supported types are \"reject\" and \"archive\" " << std::endl;
+    }
+    
+    const std::string existing_file = filepath+filename;
+    const std::string target_file = target_path+filename;
+    
+    int res = MoveFile(existing_file.c_str(),target_file.c_str());
+    if(res==0)
+    {
+        std::cerr << "Could not move file " << existing_file << " to " << target_file << std::endl;
+        std::cerr << "Error Code:" << GetLastError();
+    }
+    return res;
+}
+
+
 int main()
 {
     //Establish connection to database
@@ -50,6 +80,10 @@ int main()
     std::vector<std::string> files = get_all_files_names_within_folder(file_path);
     std::cout << "Region files:" << files.size() << std::endl;
     
+    if(files.size() > 0)
+    {
+        move_file(file_path,files[0],"archive");
+    }
     
     PQfinish(conn);
     
