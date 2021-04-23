503cd9baaaf38966b80e259b676b0ca5e2786490
linjoehan <linjoehan@yahoo.com>
2021-04-23 08:19:55 +0200

    Implemented function to get a list of all files in a folder

--- py_solution/src/SimpleSalesProcessor.py
+++ py_solution/src/SimpleSalesProcessor.py
@@ -1,6 +1,12 @@
 import psycopg2
+import os
+
+def get_all_files_names_within_folder(folder):
+  arr = os.listdir(folder)
+  return arr
 
 conn = psycopg2.connect(database="simplesalesprocessor")
-print("Connection created")
-
 conn.close()
+
+files = get_all_files_names_within_folder("./Data/Archive/")
+print(files)
